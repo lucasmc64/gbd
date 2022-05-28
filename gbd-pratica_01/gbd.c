59626e922254d00cdea109c7e581d04b3bbc34cf
Eduardo <eduardoribeirogon@gmail.com>
2022-05-28 17:41:15 -0300

    add: insert_at_end

--- gbd-pratica_01/gbd.c
+++ gbd-pratica_01/gbd.c
@@ -26,3 +26,23 @@ void create_heap_file(FILE *file, int nro_de_registros) {
         fwrite(&a, sizeof(Aluno), 1, file);
     }
 }
+
+void isrt_at_end(FILE *file) {
+    Aluno a;
+
+    Aluno teste;
+    fseek(file, -sizeof(Aluno), SEEK_END);
+    fread(&teste, sizeof(Aluno), 1, file);
+
+    a.seq_aluno = teste.seq_aluno + 1;
+    generate_string(a.codigo_curso, 3);
+    generate_string(a.nome_aluno, 43);
+
+    printf("Novo aluno a ser adicionado: \nAluno %d:\nCurso: %s\nNome: %s\n", a.seq_aluno, a.codigo_curso, a.nome_aluno);
+    printf("\n");
+
+
+    fwrite(&a, sizeof(Aluno), 1, file);
+}
+
+
